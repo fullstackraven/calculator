a29bb6ab0080e4b09f0d1485a7822e46bc6df998
fullstackraven <fullstackraven@gmail.com>
2022-11-12 23:02:26 -0500

    Add basic functionality - minor updates to follow

--- script.js
+++ script.js
@@ -1,11 +1,182 @@
-// const numbers = document.querySelectorAll('.numbers');
-// const display = document.querySelector('.display span');
-// const signs = document.querySelectorAll('.sign');
-// const negative = document.querySelector('.negative');
-// const percent = document.querySelector('.percent');
-// const equals = document.querySelector('.equals');
-// const decimal = document.querySelector('.decimal');
-// const clear = document.querySelector('.clear');
+let firstValue = "";
+let secondValue = "";
+let displayValue = '0';
+let refresh = false;
+let comma = '';
+
+const numbers = document.querySelectorAll('.numbers');
+const display = document.querySelector('.display span');
+const signs = document.querySelectorAll('.sign');
+const negative = document.querySelector('.negative');
+const percent = document.querySelector('.percent');
+const equals = document.querySelector('.equals');
+const decimalPoint = document.querySelector('.decimal');
+const clear = document.querySelector('.clear');
+const commas = document.querySelectorAll('#comma');
+
+window.addEventListener('keydown', keyInput);
+negative.addEventListener('click', outputNegative);
+percent.addEventListener('click',outputPercent);
+decimalPoint.addEventListener('click', outputDecimal);
+clear.addEventListener('click', clearDisplay);
+equals.addEventListener('click', evaluate);
+
+numbers.forEach((button) =>
+    button.addEventListener('click', () => outputNumber(button.textContent)));
+
+signs.forEach((button) =>
+    button.addEventListener('click', () => operation(button.textContent)));
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function divide(a,b) {
+    return a / b; 
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function operate(op,a,b) {
+    a = Number(a);
+    b = Number(b);
+    switch (op){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '×':
+            return multiply(a, b);
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b);
+        default:
+            return null;
+    }
+}
+
+function expo(x,f) {
+    return Number.parseFloat(x).toExponential(f);
+}
+
+function outputNumber(number) {
+    if (display.textContent === '0' || refresh)
+        refreshScreen();
+    display.textContent += number;
+    checkDisplayLength();
+}
+
+function outputDecimal() {
+    if (refresh) refreshScreen();
+    if (display.textContent === '')
+        display.textContent = '0'
+    if (display.textContent.includes('.'))  return
+        display.textContent += '.'
+}
+
+function outputPercent() {
+    operation();
+    if(display.textContent != '') {
+        display.textContent = display.textContent/100;
+    }
+}
+
+function outputNegative() {
+    operation();
+    if(display.textContent != '') {
+        display.textContent = -display.textContent;
+    }
+}
+
+function operation(operator) {
+    if (displayValue !== '0') evaluate()
+    firstValue = display.textContent;
+    displayValue = operator;
+    refresh = true;
+}
+  
+function evaluate() {
+    if (displayValue === '0' || refresh) return
+    if (displayValue === '÷' && display.textContent === '0') {
+        return display.textContent = "Nope";
+    }
+    secondValue = display.textContent;
+    display.textContent = roundOutput(operate(displayValue, firstValue, secondValue));
+    checkDisplayLength();
+}
+
+function roundOutput(number) {
+    return Math.round(number * 1000) / 1000;
+}
+
+function checkDisplayLength() {
+    if(display.textContent.length > 10 || displayValue.length > 10) {
+        display.textContent = expo(display.textContent, 4);
+        display.style.fontSize = '65px';
+    
+    } else if(display.textContent.length >= 9 || displayValue.length >= 9) {
+        display.textContent = display.textContent.substring(0,9);
+        display.style.fontSize = '65px';
+    
+    } else if(display.textContent.length >= 8 || displayValue.length >= 8){
+        display.style.fontSize = '75px';
+
+    } else if(display.textContent.length >= 7 || displayValue.length >= 7){
+        display.style.fontSize = '85px';
+
+    } else if(display.textContent.length <= 6 || displayValue.length <= 6){
+        display.style.fontSize = '100px';
+
+    }
+        
+}
+  
+function keyInput(e) {
+    if (e.key === '.') outputDecimal();
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    if (e.key >= 0 && e.key <= 9) outputNumber(e.key);
+    if (e.key === 'Escape') clearDisplay();
+    if (e.key === 'Backspace') deleteInput();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+      operation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'; 
+    if (keyboardOperator === '*') return '×'; 
+    if (keyboardOperator === '-') return '-'; 
+    if (keyboardOperator === '+') return '+'; 
+}
+
+function deleteInput() {
+    display.textContent = display.textContent
+      .toString()
+      .slice(0, -1);
+    if(display.textContent === '') {
+        display.textContent = displayValue;
+        clearDisplay();
+    }
+}
+
+function refreshScreen() {
+    display.textContent = '';
+    refresh = false;
+}
+
+function clearDisplay() {
+    display.style.fontSize = '100px';
+    display.textContent = '0';
+    firstValue = "";
+    secondValue = "";
+    displayValue = '0';
+}
+
 
 
 // let firstValue = "";
@@ -93,7 +264,7 @@
 //         displayValue = -firstValue;
 //         firstValue = displayValue;
 //     }
-//     if(firstValue != "" && secondValue != "" && sign != ""){
+//     if(firstValue != "" && secondValue != "" && signs != ""){
 //         displayValue = -displayValue;
 //     }
 
@@ -106,7 +277,7 @@
 //         displayValue = firstValue / 100;
 //         firstValue = displayValue;
 //     }
-//     if(firstValue != "" && secondValue != "" && sign != ""){
+//     if(firstValue != "" && secondValue != "" && signs != ""){
 //         displayValue = displayValue / 100;
 //     }
 
